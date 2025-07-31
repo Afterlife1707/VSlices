325fde2f047dd0f4eb2f82f25dca29014d1d7fdd
Hritick Buragohain <bestfighter17@gmail.com>
2025-07-31 20:54:43 +0100

    Slide loop animations

---
+++ Source/VSlices/Public/LoggingMacros.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+// Include the standard UE log header if not already
+#include "CoreMinimal.h"
+
+#define LOG_Warning(Format, ...) UE_LOG(LogTemp, Warning, TEXT(Format), ##__VA_ARGS__)
+#define LOG_Error(Format, ...)   UE_LOG(LogTemp, Error, TEXT(Format), ##__VA_ARGS__)
+#define LOG_Info(Format, ...)    UE_LOG(LogTemp, Display, TEXT(Format), ##__VA_ARGS__)
