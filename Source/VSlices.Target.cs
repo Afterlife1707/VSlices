168945073a3238f3a570324df5d9857f2f994f9a
Hritick Buragohain <bestfighter17@gmail.com>
2026-03-10 19:15:34 +0000

    Version upgrade to 5.7

--- Source/VSlices.Target.cs
+++ Source/VSlices.Target.cs
@@ -1,15 +1,14 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class VSlicesTarget : TargetRules
 {
 	public VSlicesTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("VSlices");
-	}
+    }
 }
--- Source/VSlicesEditor.Target.cs
+++ Source/VSlicesEditor.Target.cs
@@ -8,8 +8,8 @@ public class VSlicesEditorTarget : TargetRules
 	public VSlicesEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("VSlices");
 	}
 }
